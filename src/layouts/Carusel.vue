792b2f009209f98da9c9676d985209b5aab888ea
Bunyod <bunyodbunyod035@gmail.com>
2022-04-12 11:16:37 +0500

    add img

--- src/layouts/Carusel.vue
+++ src/layouts/Carusel.vue
@@ -1,39 +1,43 @@
 <template>
-  <swiper :modules="modules" :pagination="{ clickable: true }">
-    <swiper-slide class="cols">Slide 1</swiper-slide>
-    <swiper-slide class="cols">Slide 2</swiper-slide>
-    <swiper-slide class="cols">Slide 3</swiper-slide>
-  </swiper>
+  <q-carousel
+    class="carusel"
+    swipeable
+    animated
+    v-model="slide"
+    thumbnails
+    infinite
+  >
+    <q-carousel-slide :name="1" img-src="/foto/photo_2022-04-12_11-09-34.jpg" />
+    <q-carousel-slide
+      :name="2"
+      img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+    />
+    <q-carousel-slide
+      :name="3"
+      img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+    />
+    <q-carousel-slide
+      :name="4"
+      img-src="https://cdn.quasar.dev/img/quasar.jpg"
+    />
+  </q-carousel>
 </template>
 
 <script>
-import SwiperClass, { Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-
-// import swiper module styles
-import "swiper/css";
-import "swiper/css/pagination";
-// more module style...
-
+import { ref } from "vue";
 export default {
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
+  components: {},
   setup() {
     return {
-      modules: [Pagination],
+      slide: ref(1),
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.card {
-  .cols {
-    width: 100px;
-    height: 100px;
-    background-color: #000;
-  }
+.carusel {
+  width: 100%;
+  height: 75vh;
 }
 </style>
